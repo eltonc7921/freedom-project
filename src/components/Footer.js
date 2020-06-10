c58e7dad401ebc56fa1d70fae29d9a43b22f31a0
eltonc7921 <eltonchan1@yahoo.com>
2020-06-10 01:29:23 -0400

    laws of motion

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ class Footer extends React.Component {
 	render() {
 		return (
 			<footer className="Footer">
+					<p> eltonc7921 </p>
 			</footer>
 		);
 	}
--- src/components/articles/MotionDescription.js
+++ src/components/articles/MotionDescription.js
@@ -3,7 +3,17 @@ import React from 'react';
 const MotionDescription = {
 	title : 'Motion',
 	article : <div>
-	</div>,
+		<p>
+			Newton's first law of motion<br/>
+			A body at rest will remain at rest and a body in motion will remain in motion unless acted upon an external force<br/>
+			<br/>
+			Newton's second law of motion<br/>
+			F = ma, Force = mass * acceleration<br/>
+			Newton's third law of motion<br/>
+			<br/>
+			When a body exerts a force on another body, that other body excerts an opposite force of the same magnitude<br/>
+		</p>
+	</div>
 }
 
 export default MotionDescription;
