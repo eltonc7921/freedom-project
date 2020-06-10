b1b7e657232aafee2264d223ddf92715e7140007
eltonc7921 <eltonchan1@yahoo.com>
2020-06-10 01:44:56 -0400

    Gravity article

--- src/components/articles/GravityDescription.js
+++ src/components/articles/GravityDescription.js
@@ -3,7 +3,16 @@ import React from 'react';
 const GravityDescription = {
 	title : 'Gravity',
 	article : <div>
-		<p> gravity </p>
+		<p>
+			Gravity as described by Newton<br/>
+			is proportional to the mass of objects<br/>
+			and inversely proportional to the distance<br/>
+			between those objects<br/>
+			<br/>
+			The formula is as follows<br/>
+			F = G*m1*m2/(r^2)<br/>
+			where G is the gravitational constant<br/>
+		</p>
 	</div>,
 }
 
